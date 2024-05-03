ca66b8a6368370385266dac40f812a3f9f1479bd
soizhiwen <zhiwen39@gmail.com>
2024-05-03 08:55:46 +0200

    Refactor and delete files

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
-# python main.py --name {name} --csv_file {data.csv} \
-#     --config_file {config.yaml} --gpu 0 --train
-
-python main.py --name exp_1 --csv_file ./Data/datasets/stock_data.csv \
-    --config_file ./Config/stocks_latent.yaml --gpu 0 --train
+python sim.py --name exp_1 --num_clients 3 \
+    --num_clusters 2 --num_rounds 2 \
+    --config_file ./Config/stocks_hfl.yaml \
+    --num_cpus 6 --num_gpus 0.8 --multi_avg
