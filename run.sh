6d95ed7f1f8458c4c9a54c9500ea706eb7380fb8
soizhiwen <zhiwen39@gmail.com>
2024-05-10 20:39:31 +0200

    Add energy dataset

--- run.sh
+++ run.sh
@@ -1,26 +1,30 @@
+###############################################################################
+# Stock Dataset
+###############################################################################
+
 # FedAvg without mask
-python sim.py --name exp_1 --num_clients 3 \
-    --num_rounds 2 --split_type balance_label \
+python sim.py --name exp_1 --num_clients 10 \
+    --num_clients 20 --split_type balance_label \
     --config_file ./Config/stocks_hfl.yaml \
     --num_cpus 16 --num_gpus 0.9 --strategy fedavg
 
 # FedAvg with mask
-python sim.py --name exp_2 --num_clients 3 \
-    --full_ratio 0.2 --num_rounds 2 \
+python sim.py --name exp_2 --num_clients 10 \
+    --full_ratio 0.2 --num_clients 20 \
     --split_type balance_label \
     --config_file ./Config/stocks_hfl_mask.yaml \
     --num_cpus 16 --num_gpus 0.9 --strategy fedavg
 
 # FedWeightedAvg
-python sim.py --name exp_3 --num_clients 3 \
-    --full_ratio 0.2 --num_rounds 2 \
+python sim.py --name exp_3 --num_clients 10 \
+    --full_ratio 0.2 --num_clients 20 \
     --split_type balance_label \
     --config_file ./Config/stocks_hfl_mask.yaml \
     --num_cpus 16 --num_gpus 0.9 --strategy fedweightedavg
 
 # FedNoAvg
-python sim.py --name exp_4 --num_clients 3 \
-    --full_ratio 0.2 --num_rounds 2 \
+python sim.py --name exp_4 --num_clients 10 \
+    --full_ratio 0.2 --num_clients 20 \
     --split_type balance_label \
     --config_file ./Config/stocks_hfl_mask.yaml \
     --num_cpus 16 --num_gpus 0.9 --strategy fednoavg
@@ -28,8 +32,45 @@ python sim.py --name exp_4 --num_clients 3 \
 # FedHomoAvg
 python sim.py --name exp_5 --num_clients 10 \
     --full_ratio 0.2 --repeat_thold 0.5 \
-    --num_rounds 2 --split_type balance_label \
+    --num_clients 20 --split_type balance_label \
     --config_file ./Config/stocks_hfl_mask.yaml \
     --num_cpus 16 --num_gpus 0.9 --strategy fedhomoavg
 
-# TODO: Add energy dataset
+
+###############################################################################
+# Energy Dataset
+###############################################################################
+
+# FedAvg without mask
+python sim.py --name exp_6 --num_clients 10 \
+    --num_clients 20 --split_type balance_label \
+    --config_file ./Config/energy_hfl.yaml \
+    --num_cpus 16 --num_gpus 0.9 --strategy fedavg
+
+# FedAvg with mask
+python sim.py --name exp_7 --num_clients 10 \
+    --full_ratio 0.2 --num_clients 20 \
+    --split_type balance_label \
+    --config_file ./Config/energy_hfl_mask.yaml \
+    --num_cpus 16 --num_gpus 0.9 --strategy fedavg
+
+# FedWeightedAvg
+python sim.py --name exp_8 --num_clients 10 \
+    --full_ratio 0.2 --num_clients 20 \
+    --split_type balance_label \
+    --config_file ./Config/energy_hfl_mask.yaml \
+    --num_cpus 16 --num_gpus 0.9 --strategy fedweightedavg
+
+# FedNoAvg
+python sim.py --name exp_9 --num_clients 10 \
+    --full_ratio 0.2 --num_clients 20 \
+    --split_type balance_label \
+    --config_file ./Config/energy_hfl_mask.yaml \
+    --num_cpus 16 --num_gpus 0.9 --strategy fednoavg
+
+# FedHomoAvg
+python sim.py --name exp_10 --num_clients 10 \
+    --full_ratio 0.2 --repeat_thold 0.5 \
+    --num_clients 20 --split_type balance_label \
+    --config_file ./Config/energy_hfl_mask.yaml \
+    --num_cpus 16 --num_gpus 0.9 --strategy fedhomoavg
